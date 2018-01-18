6ad82d11a5482891a207aa4b22c0421763c9d1ee
Henri Pallasaho <henripal@ety-a2123b-26.edu.wks.metropolia.fi>
2018-01-18 16:16:45 +0200

    second commit

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -1,30 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-http-test',
-  templateUrl: './http-test.component.html',
-  styleUrls: ['./http-test.component.scss']
+    selector: 'app-http-test',
+    templateUrl: './http-test.component.html',
+    styleUrls: ['./http-test.component.scss']
 })
 export class HttpTestComponent implements OnInit {
 
-  tulos  = 'Moro';
+    tulos = 'Moro';
+    apitulos = 'Moro taas';
+    apiosoite = 'http://media.mw.metropolia.fi/wbma';
+    kuvaosoite = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
-  constructor(private http: HttpClient) { }
+    osoitetulos: any;
+    osoiteapi = 'http://api.hel.fi/linkedevents/v1/event/?format=json';
 
-  getJson() {
-    interface Myinterface {
-      license: string;
+    constructor(private http: HttpClient) {
     }
 
-    this.http.get<Myinterface>('assets/package.json').subscribe((data) => {
-      console.log(data);
-      this.tulos = data.license;
-    });
-  }
+    getJson() {
+        interface Myinterface {
+            license: string;
+        }
 
-  ngOnInit() {
-    this.getJson();
-  }
+        this.http.get<Myinterface>('assets/package.json').subscribe((data) => {
+            console.log(data);
+            this.tulos = data.license;
+        });
+    }
+
+    getFromApi() {
+        this.http.get(this.apiosoite + '/media').subscribe(data => {
+            console.log(data[0].filename);
+            this.apitulos = this.kuvaosoite + data[0].filename;
+        });
+    }
+
+    getFromApi2() {
+        interface Myinterface {
+            data: any;
+        }
+
+        this.http.get<Myinterface>(this.osoiteapi).subscribe(resp => {
+            console.log(resp.data);
+            this.osoitetulos = resp.data;
+        });
+    }
+
+    ngOnInit() {
+        this.getJson();
+        this.getFromApi();
+        this.getFromApi2();
+    }
 
 }
